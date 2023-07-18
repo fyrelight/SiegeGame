9a023cb5d12f931c39240fab1c111cf2f5274309
lexiccn <13711345+lexiccn@users.noreply.github.com>
2023-07-18 18:27:59 +0100

    Add sounds when buying items

--- src/main/java/me/cedric/siegegame/display/shop/Buyable.java
+++ src/main/java/me/cedric/siegegame/display/shop/Buyable.java
@@ -3,6 +3,7 @@ package me.cedric.siegegame.display.shop;
 import me.cedric.siegegame.model.teams.Team;
 import me.cedric.siegegame.player.GamePlayer;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -19,6 +20,7 @@ public interface Buyable {
 
         if (!gamePlayer.hasTeam()) {
             player.sendMessage(ChatColor.RED + "You need to be in a team to buy items");
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
             return false;
         }
 
@@ -26,22 +28,26 @@ public interface Buyable {
 
         if (!team.getTerritory().isInside(player.getLocation())) {
             player.sendMessage(ChatColor.RED + "You need to be inside claims to do this");
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
             return false;
         }
 
         if (gamePlayer.isDead()) {
             player.sendMessage(ChatColor.RED + "You cannot do this while dead");
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
             return false;
         }
 
         if (player.getLevel() < getPrice()) {
             player.sendMessage(ChatColor.RED + "You do not have enough levels to buy this");
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
             return false;
         }
 
         int levels = player.getLevel();
         player.setLevel(levels - getPrice());
         getPurchase().accept(gamePlayer);
+        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1, 1);
         return true;
     }
 
