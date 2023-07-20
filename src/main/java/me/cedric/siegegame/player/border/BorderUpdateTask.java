1fa813b55b10c2d6b9df301fc9a3f6ef4aacb9fe
lexiccn <13711345+lexiccn@users.noreply.github.com>
2023-07-20 18:00:54 +0100

    Fix borders

--- src/main/java/me/cedric/siegegame/player/border/BorderUpdateTask.java
+++ src/main/java/me/cedric/siegegame/player/border/BorderUpdateTask.java
@@ -1,18 +1,25 @@
 package me.cedric.siegegame.player.border;
 
+import me.cedric.siegegame.SiegeGamePlugin;
 import me.cedric.siegegame.enums.Permissions;
+import me.cedric.siegegame.model.GameManager;
+import me.cedric.siegegame.model.SiegeGameMatch;
 import me.cedric.siegegame.player.GamePlayer;
 import me.cedric.siegegame.player.border.blockers.EntityTracker;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.UUID;
+
 public class BorderUpdateTask extends BukkitRunnable {
 
-    GamePlayer player;
+    private final GameManager manager;
+    private final UUID uuid;
 
-    public BorderUpdateTask(GamePlayer player) {
-        this.player = player;
+    public BorderUpdateTask(GameManager manager, UUID uuid) {
+        this.manager = manager;
+        this.uuid = uuid;
     }
 
     private boolean shouldCheck(GamePlayer gamePlayer) {
@@ -27,12 +34,21 @@ public class BorderUpdateTask extends BukkitRunnable {
 
     @Override
     public void run() {
-        if (player.getBukkitPlayer() == null || !player.getBukkitPlayer().isOnline()) {
-            cancel();
+        SiegeGameMatch match = manager.getCurrentMatch();
+
+        if (match == null)
             return;
-        }
-        if (!shouldCheck(player))
+
+        GamePlayer gamePlayer = match.getWorldGame().getPlayer(uuid);
+
+        if (gamePlayer == null)
             return;
-        player.getBorderHandler().getBorders().forEach(border -> player.getBorderHandler().getBorderDisplay(border).update());
+
+        if (!shouldCheck(gamePlayer))
+            return;
+
+        PlayerBorderHandler handler = gamePlayer.getBorderHandler();
+
+        handler.getBorders().forEach(border -> handler.getBorderDisplay(border).update());
     }
 }
